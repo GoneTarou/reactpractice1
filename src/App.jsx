b4126b276ca8330cf0efd5846703afbe9c075f2b
GoneTarou <konnpen@gmail.com>
2026-05-01 20:08:10 +0900

    amend app.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,42 @@ import './App.css'
 
 export const Todo = () => {
   return (
-    <div className="App">
-      <h1>Todo App</h1>
+    <div>
+      <div>
+        <h1>Todo App</h1>
+        <input placeholder="Todoを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTodo</p>
+        <ul>
+          <li>
+            <p>Todoです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li>
+            <p>Todoです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTodo</p>
+        <ul>
+          <li>
+            <p>Todoでした</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li>
+            <p>Todoです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 };
