caaef8524d8d132227e9719dc3c141efc4d31afb
GoneTarou <konnpen@gmail.com>
2026-05-03 01:54:03 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,13 @@
 import './App.css'
 import { useState } from 'react';
-import { InputTodo } from './components/InputTodo';
-import { IncompleteTodo } from './components/IncompleteTodo';
-import { CompleteTodo } from './components/CompleteTodo';
 
-
-export const Todo = () => {
-  const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState(["Todo1", "Todo2"]);
-  const [completeTodos, setCompleteTodos] = useState(["Todoでした1", "Todoでした2"]);
-
-  const onChangeText = (e) => setTodoText(e.target.value);
-  const onClickAdd = () => {
-    if (todoText === "") return;
-    const newTodos = [...incompleteTodos, todoText];
-    setIncompleteTodos(newTodos);
-  };
-  const onClickDelete = (index) => {
-    const newTodos = [...incompleteTodos];
-    newTodos.splice(index, 1);
-    setIncompleteTodos(newTodos);
-  };
-  const onClickComplete = (index) => {
-    const newIncompleteTodos = [...incompleteTodos];
-    newIncompleteTodos.splice(index, 1);
-
-    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
-    setIncompleteTodos(newIncompleteTodos);
-    setCompleteTodos(newCompleteTodos);
-  };
-
-  const onClickBack = (index) => {
-    const newCompleteTodos = [...completeTodos];
-    newCompleteTodos.splice(index,1);
-
-    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
-    setCompleteTodos(newCompleteTodos);
-    setIncompleteTodos(newIncompleteTodos);
-  };
-
-  const isMax = incompleteTodos.length >= 2;
+export const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <InputTodo todoText={todoText} onChange={onChangeText} onClick={onClickAdd}
-        disabled={isMax} />
-      {isMax && (
-        <p style={{ color: "red" }}>surti</p>
-      )}
-      <IncompleteTodo todos={incompleteTodos} onClickComplete={onClickComplete} onClickDelete={onClickDelete} />
-      <CompleteTodo completeTodos={completeTodos} onClickBack={onClickBack} />
+    <div className='App'>
+      <h1>app</h1>
+      <p>{count}</p>
     </div>
   )
 };
