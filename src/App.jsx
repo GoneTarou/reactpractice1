2724394f6d1b44a3d9317da86b9574072578fd3e
GoneTarou <konnpen@gmail.com>
2026-05-03 02:17:05 +0900

    b

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import { useState } from 'react';
 export const App = () => {
   const [count, setCount] = useState(0);
 
+  const onClickCountUp = () => {
+    setCount(count + 1);
+  }
+
   return (
     <div className='App'>
       <h1>app</h1>
       <p>{count}</p>
+      <button onClick={onClickCountUp}>countup</button>
     </div>
   )
 };
