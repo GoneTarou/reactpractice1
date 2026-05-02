ba8480a291807ec8312f26c5605dd0cf11974a73
GoneTarou <konnpen@gmail.com>
2026-05-02 17:36:50 +0900

    b

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,19 @@ export const Todo = () => {
   const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["Todo1", "Todo2"]);
   const [completeTodos, setCompleteTodos] = useState(["Todoでした1", "Todoでした2"]);
-  const onChangeText = (event) => setTodoText(event.target.value);
+  const onChangeText = (e) => setTodoText(e.target.value);
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+  };
 
   return (
     <div>
       <div>
         <h1>Todo App</h1>
         <input placeholder="Todoを入力" value={todoText} onChange={onChangeText}/>
-        <button>追加</button>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div>
         <p>未完了のTodo</p>
@@ -22,7 +27,7 @@ export const Todo = () => {
               <li key={todo}>
                 <div className='list-row'>
                   <p className='todo-item'>{todo}</p>
-                  <button>完了</button>
+                  <button onClick={onClickAdd}>完了</button>
                   <button>削除</button>
                 </div>
               </li>
