f2a9fbc65fe815a9c337c233a1a12635a1575a5e
GoneTarou <konnpen@gmail.com>
2026-05-03 17:57:35 +0900

    e

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,12 +1,21 @@
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+}
+
 export const ChildArea = (props) => {
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {})
   const { open } = props;
   return (
     <div>
-        {open ? (
-        <div>
+        {open && (
+        <div style={style}>
           <p>surti</p>
         </div>
-        ) : null}
+        )}
     </div>
   );
 };
