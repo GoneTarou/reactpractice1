8f692df87ccbe35f2e4e76099d935bf881a847ea
GoneTarou <konnpen@gmail.com>
2026-05-04 20:51:14 +0900

    a

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,8 +1,16 @@
-export const Question = (props) => {
+import { Question } from "../data/question";
+
+export const QuestionWord = (props) => {
   const { question, currentQuestionIndex, totalQuestions, onAnswer } = props;
   return (
-    <div>
-      <h2>{question.text}</h2>
+    <div className="min-h-screen bg-orange-50 py-8 px-4">
+      {/* <Progress
+        current={currentQuestionIndex + 1}
+        total={totalQuestions}
+      /> */}
+      <div>
+        <h1>{question.text}</h1>
+      </div>
     </div>
   );
 };
--- src/data/question.jsx
+++ src/data/question.jsx
@@ -1,16 +1,20 @@
-export const question = [
+export const questions = [
   {
     id: 1,
     text: "猫が好きか犬が好きか",
-    answer: [
+    answers: [
       {
         text: "猫",
-        scores: { a: 1, b: 0, c:0 },
-        text: "犬",
-        scores: { a:0, b:1, c:0 },
-        text: "それ以外",
-        scores: { a:0, b:0, c:1 }
+        scores: { a: 1, b: 0, c: 0 }
       },
+      {
+        text: "犬",
+        scores: { a: 0, b: 1, c: 0 }
+      },
+      {
+        text: "それ以外",
+        scores: { a: 0, b: 0, c: 1 }
+      }
     ]
   }
 ];
