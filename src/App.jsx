e1aa8630ddb4faa5bff73d1c9b718d9f2f72ab2d
GoneTarou <konnpen@gmail.com>
2026-05-04 14:24:24 +0900

    make components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+
+// import StartPage from "./components/StartPage";
+
 export default function App() {
   return (
-    <div className="text-5xl text-red-500">
-      Tailwind効いてる？
+    <div className="p-4 m-2">
+      <p className="mb-4">マージン下</p>
+      <p className="px-2">パディング左右</p>
     </div>
   );
 }
