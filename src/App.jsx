0be4a844238b0f136f14056126ab0fb02621d393
GoneTarou <konnpen@gmail.com>
2026-05-04 16:46:52 +0900

    named export StartPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from "react";
+import { StartPage } from "./components/StartPage";
 
-// import StartPage from "./components/StartPage";
-
-export default function App() {
+const App = () => {
   return (
     <div className="p-4 m-2">
-      <p className="mb-4">マージン下</p>
-      <p className="px-2">パディング左右</p>
+      <StartPage />
     </div>
   );
-}
+};
+
+export default App;
--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,3 +1,11 @@
-import React from "react";
 
-const StartPage = ({onStart}) => {};
+
+export const StartPage = () => {
+  return (
+    <div>
+      <div>
+        <h1>猫ミーム診断</h1>
+      </div>
+    </div>
+  );
+};
